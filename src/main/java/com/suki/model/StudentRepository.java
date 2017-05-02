c772532ca9c4fe549f00e73940037e3cf918e8b2
Kinnunen Suzan <a1503022@myy.haaga-helia.fi>
2017-05-02 10:47:02 +0300

    Projekti studentin pohjalle startedededed

---
+++ src/main/java/com/suki/model/StudentRepository.java
@@ -0,0 +1,9 @@
+package com.suki.model;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface StudentRepository extends CrudRepository<Student, Long> {
+    List<Student> findByLastName(String lastName);
+}
---
+++ src/test/java/com/suki/StudentRestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.suki;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StudentRestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
